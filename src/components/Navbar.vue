e83e543d30deb48cd4951b9b2a2d443bdf4b6f0f
Julius Blezinger <jgoldappel@gmail.com>
2023-08-29 03:57:10 +0200

    adapts navbar for desktop

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,20 @@
 <template>
   <nav class="fixed w-full bg-white top-0 md:h-20 h-16 lg:h-16 z-10">
     <div>
-      <div class="">
+      <div class="lg:flex lg:justify-between">
         <router-link to="/" class="flex flex-row" @click="isOpen = false">
           <img :src="logoImage" alt="Logo" class="md:h-12 md:w-12 h-8 w-8 lg:h-8 lg:w-8 m-4">
-          <span class="self-center text-xl">NewGen</span>
+          <span class="self-center text-xl lg:py-5 lg:font-medium">NewGen</span>
         </router-link>
+        <div class="flex hidden lg:block p-8 space-x-8 py-5 font-medium">
+          <router-link active-class="text-main" to="/">HOME</router-link>
+          <router-link active-class="text-main" to="/festival">FESTIVAL</router-link>
+          <router-link active-class="text-main" to="/about-us">ABOUT US</router-link>
+          <router-link active-class="text-main" to="/join-us">JOIN US</router-link>
+          <router-link active-class="text-main" to="/submissions">SUBMISSIONS</router-link>
+          <router-link active-class="text-main" to="/contact">CONTACT</router-link>
+          <router-link active-class="text-main" to="/archive">ARCHIVE</router-link>
+        </div>
       </div>
       <div>
         <!-- Hamburger & Close Icon -->
@@ -18,9 +27,11 @@
           </div> <!-- You can replace this with an X SVG/icon -->
         </div>
 
+
         <!-- Navigation Links -->
         <div v-if="isOpen" class="bg-white fixed top-16 text-center w-full h-full">
           <ul class="list-none p-8 mt-12">
+            <li class="text-2xl md:text-4xl mb-4 md:mb-16" @click="isOpen = !isOpen"><router-link to="/">HOME</router-link></li>
             <li class="text-2xl md:text-4xl mb-4 md:mb-16" @click="isOpen = !isOpen"><router-link to="/festival">FESTIVAL</router-link></li>
             <li class="text-2xl md:text-4xl mb-4 md:mb-16" @click="isOpen = !isOpen"><router-link to="/about-us">ABOUT US</router-link></li>
             <li class="text-2xl md:text-4xl mb-4 md:mb-16" @click="isOpen = !isOpen"><router-link to="/join-us">JOIN US</router-link></li>
