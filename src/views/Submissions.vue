0fade5e511946b46e693a553bac365f6b2bbfa00
Julius Blezinger <jgoldappel@gmail.com>
2023-08-30 19:38:13 +0200

    adapts Submissions for Desktop

--- src/views/Submissions.vue
+++ src/views/Submissions.vue
@@ -11,16 +11,16 @@
     </template>
   </HeroSection>
   <section class="mt-8 mb-8 md:my-16">
-    <div class="px-8 md:px-16">
-      <h3 class="text-main text-3xl md:text-4xl md:font-semibold  italic font-medium mb-2">Submission Deadlines & Fees</h3>
-      <p class="font-medium text-lg md:text-xl">May 7, 2023 (CET) - 1€</p>
-      <p class="font-medium text-lg md:text-xl">June 30, 2023 (CET) - 3€</p>
+    <div class="px-8 md:px-16 lg:px-24">
+      <h3 class="text-main text-3xl md:text-4xl lg:text-5xl md:font-semibold  italic font-medium mb-2">Submission Deadlines & Fees</h3>
+      <p class="font-medium text-lg md:text-xl lg:text-2xl ">May 7, 2023 (CET) - 1€</p>
+      <p class="font-medium text-lg md:text-xl lg:text-2xl ">June 30, 2023 (CET) - 3€</p>
     </div>
   </section>
   <section class="mt-8 mb-8 md:my-16">
-    <div class="px-8 md:px-16">
-      <h3 class="text-main text-3xl md:text-4xl md:font-semibold  italic font-medium mb-2">Terms & Conditions</h3>
-      <ol class="font-medium text-lg md:text-xl list-decimal px-6">
+    <div class="px-8 md:px-16 lg:px-24">
+      <h3 class="text-main text-3xl md:text-4xl lg:text-5xl md:font-semibold  italic font-medium mb-2">Terms & Conditions</h3>
+      <ol class="font-medium text-lg md:text-xl lg:text-2xl list-decimal px-6">
         <li>
           <div>The submitted film must</div>
           <ol class="list-decimal pl-6 mt-2">
@@ -39,13 +39,13 @@
     </div>
   </section>
   <section class="mt-8 mb-8 md:my-16">
-    <div class="px-8 md:px-16">
-      <h3 class="text-main text-3xl md:text-4xl md:font-semibold  italic font-medium mb-2">How To Submit</h3>
-      <p class="font-medium text-lg md:text-xl">To apply, please fill out the application form and submit your film on <a href="https://filmfreeway.com/" target="_blank">FilmFreeway</a>.<br><br></p>
-      <p class="font-medium text-lg md:text-xl">In principle, we do not accept new versions after your successful submission. In case of major changes, please contact info@bngcff.com before the regular submission deadline.</p>
+    <div class="px-8 md:px-16 lg:px-24">
+      <h3 class="text-main text-3xl md:text-4xl lg:text-5xl md:font-semibold italic font-medium mb-2">How To Submit</h3>
+      <p class="font-medium text-lg md:text-xl lg:text-2xl ">To apply, please fill out the application form and submit your film on <a href="https://filmfreeway.com/" target="_blank">FilmFreeway</a>.<br><br></p>
+      <p class="font-medium text-lg md:text-xl lg:text-2xl ">In principle, we do not accept new versions after your successful submission. In case of major changes, please contact info@bngcff.com before the regular submission deadline.</p>
     </div>
   </section>
-  <div class="px-8 md:px-16 mb-16">
+  <div class="px-8 md:px-16 lg:px-24 mb-16 lg:mb-32">
     <p class="mb-2 font-medium text-lg md:text-xl md:font-bold">Submit on:</p>
     <button class="bg-main rounded-lg"><a href="https://filmfreeway.com/" target="_blank"><img src="@/assets/filmfreeway-logo-hires-black.png" alt=""></a></button>
   </div>
